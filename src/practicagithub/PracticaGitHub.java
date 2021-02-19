ab0d9f75ff29efabc52a9c0f5d2faad287635270
CristianAguayo23 <cristianmat23america@gmail.com>
2021-02-19 13:18:02 -0600

    Esta es mi primera versión

---
+++ src/practicagithub/PracticaGitHub.java
@@ -0,0 +1,21 @@
+
+package practicagithub;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CRIS
+ */
+public class PracticaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+            JOptionPane.showMessageDialog(null,"Hola mundo");
+
+    }
+    
+}
